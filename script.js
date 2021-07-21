01b43560682a61c6da59e6f02b3e1132c1e51b40
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-20 20:50:00 -0500

    linked JS

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var datetime = null,
         date = null;
+
 var update = function () {
     date = moment(new Date())
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 };
+
 $(document).ready(function(){
-    datetime = $('#datatime')
+    datetime = $('#datetime')
     update();
     setInterval(update, 1000);
 });
