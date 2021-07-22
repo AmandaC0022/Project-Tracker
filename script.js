5a29a8c184a30fe04ee75e56a7ecbb1e0ca937ad
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-22 17:04:32 -0500

    table input works

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ var update = function () {
     date = moment(new Date())
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 };
+$(document).ready(function(){
+    datetime = $('#datetime')
+    update();
+    setInterval(update, 1000);
+});
 
+//date picker 
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
@@ -17,60 +23,39 @@ projectBtn.on("click", function() {
     $(".custom-form").show(1000); 
 })
 
-$( function() {
-    $( "#datepicker" ).datepicker({
-      showOtherMonths: true,
-      selectOtherMonths: true
-    });
-  } );
-
+// When user clicks on Add project btn, then user is taken to the form 
 $(".submit-btn").on("click", function() {
-    //Project Name
-    //this only works with ids! 
+    //Project Name 
     var projectValue = $("#project-name").val(); 
-    // var projectValueLS = localStorage.setItem("Project Name", projectValue); 
-    // I can only store 1 value in local storage at the moment. To store multiple use an array or Object. Turn them into a string and then store 
-    $('#project-name-value').text(projectValue); 
+    $('#project-name-value').text(projectValue);
+
     //Project Type 
-    var projectTypeValue = $("#project-type option:selected").text(); 
-    console.log(projectTypeValue); 
+    var projectTypeValue = $("#project-type option:selected").text();  
     $('#project-type-value').text(projectTypeValue); 
+
     //Hourly Wage 
     var wageValue = $("#hourly-wage").val(); 
     $('#wage-value').text("$" + wageValue); 
-    //Due Date 
-    $("#due-date").on("click", function(){
 
-    })
+    //Due Date 
+    var dueDateValue = $("#datepicker").val();
+    $("#due-date-value").text(dueDateValue);  
+
     //hides the form and populates the table 
     $(".custom-form").hide();  
 })
 
 $('#delete').on("click", function(){
+    confirm("Are you sure you want to delete this task?"); 
     $("#project-name-value").text(" "); 
     $("#project-type-value").text(" "); 
     $("#wage-value").text(" "); 
     $("#due-date-value").text(" "); 
     $("#days-remaining").text(" "); 
-})
-
-    // When user clicks on Add project btn, then user is taken to the form 
-    //Use the show and hide jQuery animation for the form 
-
-    // User will need to fill out: 
-    //     name of project 
-    //     type of project - drop down list 
-    //     hourly wage = $15 per hour? 
-    //     Due date for project - jquery datepicker 
-    //     Submit Btn 
-
-// add a function to display form info that populates the table 
-    // When User clicks submit Btn, info is added to the table 
+}) 
 
 //add function to calculate how many days left in project 
     // days until due date - function DueDate - Current Date = how many days left 
         
 // add function to calculate total earned 
     // Total Earned = (8hours per day * Hourly Rate $15?) * how many days until due date from the time of submission NOT how many days remaining  
-
-//add a delete btn on the table with X so that when user clicks on the X btn, that row is deleted off of the table 
