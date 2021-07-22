3cb714a46e5011eb41cf7ad041ceed90dda0edc2
Amanda Morgan <AmandaC0022@gmail.com>
2021-07-21 22:21:06 -0500

    added JS psuedocode

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ $(document).ready(function(){
 });
 
 // Add a function to display form 
+    // When user clicks on Add project btn, then user is taken to the form 
+    //Use the show and hide jQuery animation for the form 
+
+    // User will need to fill out: 
+    //     name of project 
+    //     type of project - drop down list 
+    //     hourly wage = $15 per hour? 
+    //     Due date for project - jquery datepicker 
+    //     Submit Btn 
 
 // add a function to display form info that populates the table 
+    // When User clicks submit Btn, info is added to the table 
+
+//add function to calculate how many days left in project 
+    // days until due date - function DueDate - Current Date = how many days left 
+        
+// add function to calculate total earned 
+    // Total Earned = (8hours per day * Hourly Rate $15?) * how many days until due date from the time of submission NOT how many days remaining  
+
+//add a delete btn on the table with X so that when user clicks on the X btn, that row is deleted off of the table 
